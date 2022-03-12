86087d518fccc4d4c0444d26fb80324b10dd507f
Kaj0Wortel <kaj.wortel@gmail.com>
2022-03-12 15:11:03 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Word counter
+
+## Getting started
+Use `./gradlew :run` to execute the project.
+Use `./gradlew :export` to generate the Jar, JavaDoc, and zipped sources.
---
+++ src/main/java/com/ordina/word_count/Main.java
@@ -0,0 +1,12 @@
+package com.ordina.word_count;
+
+/**
+ * Main class of the application.
+ */
+public class Main {
+	
+	public static void main(String[] args) {
+		System.out.println("test");
+	}
+	
+}
